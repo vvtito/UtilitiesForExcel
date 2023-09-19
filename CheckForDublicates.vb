bd0d02ce946d20d0b2d77057f02335570e9b71fc
vvtito <vtito19880426@gmail.com>
2023-09-19 19:52:56 +0300

    Control of duplication of records in Excel slight improvement

--- CheckForDublicates.vb
+++ CheckForDublicates.vb
@@ -1,26 +1,52 @@
 Option Explicit
 Private Sub Worksheet_Change(ByVal Target As Range)
     Dim originalEntry As Range
-    Dim rowCount As Long, currentRow As Long
+    Dim rowCount As Long, currentRow As Long, currentCol As Long
     Dim key1FormulaTemplate As String, key2FormulaTemplate As String
     
-    key1FormulaTemplate = "C[Row]&E[Row]"
-    key2FormulaTemplate = "D[Row]&E[Row]"
+    key1FormulaTemplate = "C[Row]&H[Row]"
+    key2FormulaTemplate = "D[Row]&I[Row]"
     
     rowCount = ActiveSheet.Cells(ActiveSheet.Rows.Count, "A").End(xlUp).Row
     currentRow = Target.Row
+    currentCol = Target.Column
     
-    Set originalEntry = CheckForDublicates("A", key1FormulaTemplate, rowCount, currentRow)
-    If originalEntry Is Nothing Then
-     Set originalEntry = CheckForDublicates("B", key2FormulaTemplate, rowCount, currentRow)
+    If currentCol = 3 Then
+      Call TryToFindEmployee(Target, rowCount)
     End If
     
-    If Not originalEntry Is Nothing Then
-       Dim response As Integer
-       response = MsgBox("Such an entry is already in the list. would you like to see it?", vbYesNo, "Duplicate found")
-       If response = vbYes Then
-        originalEntry.Select
-       End If
+    If currentCol = 8 Or currentCol = 9 Then
+        Set originalEntry = CheckForDublicates("A", key1FormulaTemplate, rowCount, currentRow)
+        If originalEntry Is Nothing Then
+         Set originalEntry = CheckForDublicates("B", key2FormulaTemplate, rowCount, currentRow)
+        End If
+        
+        If Not originalEntry Is Nothing Then
+           Dim response As Integer
+           response = MsgBox("Such an entry is already in the list. would you like to see it?", vbYesNo, "Duplicate found")
+           If response = vbYes Then
+            originalEntry.Select
+           End If
+        End If
+    End If
+End Sub
+Sub TryToFindEmployee(changed_cell As Range, rowCount As Long)
+    Dim currentKey As String
+    
+    currentKey = changed_cell.Value2
+    
+    Dim keyRange As Range
+    Set keyRange = ActiveSheet.Range("C2:C" & rowCount)
+    Dim foundRange As Range
+    Set foundRange = keyRange.Find(currentKey, LookIn:=xlValues, LookAt:=xlWhole, SearchOrder:=xlByRows, SearchDirection:=xlNext, MatchCase:=True)
+    
+    Dim response As Integer
+    If Not (foundRange Is Nothing) Then
+      If foundRange.Row <> changed_cell.Row Then
+        Cells(changed_cell.Row, 4).Value = Cells(foundRange.Row, 4).Value
+        Cells(changed_cell.Row, 5).Value = Cells(foundRange.Row, 5).Value
+        Cells(changed_cell.Row, 6).Value = Cells(foundRange.Row, 6).Value
+      End If
     End If
 End Sub
 Function CheckForDublicates(keyColumn As String, keyFormulaTemplate As String, rowCount As Long, currentRow As Long) As Range
@@ -31,7 +57,7 @@ Function CheckForDublicates(keyColumn As String, keyFormulaTemplate As String, r
     
     For Each item In keys
      keyFormula = Replace(item, "[Row]", currentRow)
-     currentKey = currentKey + Range(keyFormula).Value2
+     currentKey = currentKey & Range(keyFormula).Value2
     Next item
     
     Dim keyRange As Range
